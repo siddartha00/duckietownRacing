27616361c1e2a4e6f34af48f895e93339c41eab0
Sai Siddartha, Alleni <allenisaisiddharth@gmail.com>
2025-10-20 15:28:25 -0400

    cde

--- packages/mypackage/src/publisher.py
+++ packages/mypackage/src/publisher.py
@@ -2,40 +2,59 @@
 
 import os
 import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge
 from duckietown.dtros import DTROS, NodeType
-from duckietown_msgs.msg import WheelsCmdStamped
+from sensor_msgs.msg import CompressedImage
+from duckietown_msgs.msg import Twist2DStamped
 
-# throttle and direction for each wheel
-THROTTLE_LEFT = 0.5        # 50% throttle
-DIRECTION_LEFT = 1         # forward
-THROTTLE_RIGHT = 0.3       # 30% throttle
-DIRECTION_RIGHT = -1       # backward
+class UVAlignControllerNode(DTROS):
+    def __init__(self, node_name):
+        super(UVAlignControllerNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
 
-class WheelControlNode(DTROS):
+     
+        self.bridge = CvBridge()
+        self.vehicle_name = os.environ['VEHICLE_NAME']
+        self.image_topic = f"/{self.vehicle_name}/camera_node/image/compressed"
+        self.cmd_topic = f"/{self.vehicle_name}/car_cmd_switch_node/cmd"
 
-   def __init__(self, node_name):
-       super(WheelControlNode, self).__init__(
-           node_name=node_name,
-           node_type=NodeType.GENERIC
-       )
-       vehicle_name = os.environ['VEHICLE_NAME']
-       wheels_topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
-       self._vel_left = THROTTLE_LEFT * DIRECTION_LEFT
-       self._vel_right = THROTTLE_RIGHT * DIRECTION_RIGHT
-       self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
+      
+        self.u_desired = 330 
+        self.v_desired = 240  
 
-   def run(self):
-       rate = rospy.Rate(0.1)
-       message = WheelsCmdStamped(vel_left=self._vel_left, vel_right=self._vel_right)
-       while not rospy.is_shutdown():
-           self._publisher.publish(message)
-           rate.sleep()
+        self.center_threshold = 5     
+        self.angular_gain = 0.005      
 
-   def on_shutdown(self):
-       stop = WheelsCmdStamped(vel_left=0, vel_right=0)
-       self._publisher.publish(stop)
+        self.publisher = rospy.Publisher(self.cmd_topic, Twist2DStamped, queue_size=1)
+        rospy.Subscriber(self.image_topic, CompressedImage, self.camera_callback)
+
+    def camera_callback(self, msg):
+       
+        image = self.bridge.compressed_imgmsg_to_cv2(msg)
+        height, width, _ = image.shape
+
+      
+        u = width // 2
+        v = height // 2
+
+        error_u = u - self.u_desired
+
+        if abs(error_u) > self.center_threshold:
+            omega = -self.angular_gain * error_u  
+        else:
+            omega = 0.0
+
+        move = Twist2DStamped(v=0.0, omega=omega)
+        self.publisher.publish(move)
+
+        rospy.loginfo(f"[u={u}] Target={self.u_desired}, Error={error_u}, Omega={omega:.3f}")
+
+    def on_shutdown(self):
+        print("stopping")
+        stop = Twist2DStamped(v=0.0, omega=0.0)
+        self.publisher.publish(stop)
 
 if __name__ == '__main__':
-   node = WheelControlNode(node_name='wheel_control_node')
-   node.run()
-   rospy.spin()
+    node = UVAlignControllerNode(node_name='uv_align_controller_node')
+    rospy.spin()
